b33247fbcae7ee150ae7440818a4fdc9dd0a3230
youngmp <ympark1988@gmail.com>
2016-12-27 19:57:56 -0500

    completed first example of parsing parameters, options, and state variables

--- examples/ex1.py
+++ examples/ex1.py
@@ -1,5 +1,39 @@
 import xppy
+import matplotlib.pyplot as plt
 
+# list all high level modules
+modlist = dir(xppy)
+print modlist
 
-# run ODE file
-xppy.run('simple.ode')
+# run ode file, catch output using class Output.
+out = xppy.Output('simple.ode')
+
+# save raw data output to convenient name
+dat = out.getRawData()
+
+# read ODE variables
+# output is a dictory of a 1-1 map of variables with corresponding index.
+# for example, variable 't' has index 0. 
+# call 0 to get the first state variable name, or call 't' to get its index.
+desc = out.getDesc()
+print desc
+
+# the above is equivalent to
+print xppy.readOdeVars('simple.ode')
+
+# read ODE params
+print xppy.readOdePars('simple.ode')
+
+# plot output
+# only the column names need to be known to assign them to a dedicated variable
+t = dat[:,desc['t']]
+u = dat[:,desc['u']]
+v = dat[:,desc['v']]
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.plot(t,u)
+ax.plot(t,v)
+plt.show()
+
